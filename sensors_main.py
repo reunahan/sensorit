3a553b0fa810cb942786ac3672e1f838f16cb30a
reunahan <rulexoo81@gmail.com>
2023-01-31 23:49:26 +0200

    yess

--- sensors_main.py
+++ sensors_main.py
@@ -3,6 +3,7 @@ import sys
 # Ensimmäinen muutos
 # This is the main finction for controlling the flow of the
 # implementation.
+#kakkos muutos
 def main():
     limits = parse_limits()
     sensor_data = []
